79ec3ed9c71243b15d879cd02d95c848bcc9522d
Facundo15 <48262116+Facundo15@users.noreply.github.com>
2023-08-28 00:17:49 -0300

    solucionado

--- eventos.js
+++ eventos.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('btn').addEventListener('click', (e) => {
+        alert('Hola')
+        e.stopPropagation();
+    })
     document.getElementById('div').addEventListener('click', () => {
         alert('Hola! Soy el div')
     })
